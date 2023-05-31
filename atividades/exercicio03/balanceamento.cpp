f7ffb25680f09096a9ca32ed7e8523e086c45e95
Gabriela Albornoz <93677092+GabiDuartte@users.noreply.github.com>
2023-05-31 00:17:30 -0300

    Create balanceamento.cpp

---
+++ atividades/exercicio03/balanceamento.cpp
@@ -0,0 +1,14 @@
+/*
+1.1 - Altura = 3
+1.2 - Árvore Resultante:         7
+                                /  \
+                               6    22
+                                   /   \
+                                  14    40
+                                          \
+                                            63
+1.3 - A árvore não está balanceada.
+1.6
+*/
+
+//1.5
